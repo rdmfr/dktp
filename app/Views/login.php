7c15ef66ab5ea3a16c151978ffa2c536e941225b
Japar Sidik <japarssidik820@gmail.com>
2022-11-03 20:08:59 +0700

    refactor: Penyesuaian Indentasi

--- app/Views/login.php
+++ app/Views/login.php
@@ -3,31 +3,31 @@ $this->extend('layouts/loginpage');
 $this->section('content');
 ?>
 <form action="" method="post">
-  <?= csrf_field() ?>
-  <img src="<?= base_url() ?>/assets/img/avatar.svg">
-  <h2 class="title"><?= $title?></h2>
-  <?= session()->getFlashdata('msg') ?>
-  <?= service('validation')->listErrors() ?>
-  <div class="input-div one">
-    <div class="i">
-      <i class="fas fa-user"></i>
-    </div>
-    <div class="div">
-      <h5>Email</h5>
-      <input type="text" class="input" name="email" id="">
-    </div>
-  </div>
-  <div class="input-div pass">
-    <div class="i">
-      <i class="fas fa-lock"></i>
-    </div>
-    <div class="div">
-      <h5>Password</h5>
-      <input type="password" class="input" name="password" id="">
-    </div>
-  </div>
-  <a href="register-page.html">Register</a>
-  <a href="#">Forgot Password?</a>
-  <input type="submit" class="btn" value="Login">
+	<?= csrf_field() ?>
+	<img src="<?= base_url() ?>/assets/img/avatar.svg">
+	<h2 class="title"><?= $title ?></h2>
+	<?= session()->getFlashdata('msg') ?>
+	<?= service('validation')->listErrors() ?>
+	<div class="input-div one">
+		<div class="i">
+			<i class="fas fa-user"></i>
+		</div>
+		<div class="div">
+			<h5>Email</h5>
+			<input type="text" class="input" name="email" id="">
+		</div>
+	</div>
+	<div class="input-div pass">
+		<div class="i">
+			<i class="fas fa-lock"></i>
+		</div>
+		<div class="div">
+			<h5>Password</h5>
+			<input type="password" class="input" name="password" id="">
+		</div>
+	</div>
+	<a href="<?= base_url('register') ?>">Register</a>
+	<a href="#">Lupa Password?</a>
+	<input type="submit" class="btn" value="Login">
 </form>
-<?= $this->endSection()?>
+<?= $this->endSection() ?>
