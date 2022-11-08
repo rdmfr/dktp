26a00432707fbf57571a426ea54023a39e99e708
Japar Sidik <japarssidik820@gmail.com>
2022-11-08 11:15:57 +0700

    feat: Pembuatan Controller utama untuk Admin/User

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -3,11 +3,21 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\User;
 
 class Admin extends BaseController
 {
     public function index()
     {
-        //
+        $data['title'] = 'Dashboard';
+        return view('dashboard',$data);
+    }
+
+    public function profile()
+    {
+        $data['title'] = 'Profile';
+        $userModel = model(User::class);
+        $data['user'] = $userModel->getSpesifikUser(['email'=>session()->get('email')]);
+        return view('profil',$data);
     }
 }
