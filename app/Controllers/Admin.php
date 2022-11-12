99c5e0c998853e607688bb9e609a0cdb8181ab86
Japar Sidik <japarssidik820@gmail.com>
2022-11-12 13:31:03 +0700

    Feat: Pengubahan Tampilan Dashboard Admin

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\Approval;
+use App\Models\Penduduk;
 use App\Models\User;
 
 class Admin extends BaseController
@@ -10,6 +12,14 @@ class Admin extends BaseController
     public function index()
     {
         $data['title'] = 'Dashboard';
+        $userModel = model(User::class);
+        $approvalModel = model(Approval::class);
+        // $pendudukModel = model(Penduduk::class);
+        $data['petugas'] = count($userModel->filterUser(['level'=>'admin']));
+        $data['penduduk'] = count($userModel->filterUser(['level'=>'user']));
+        $data['approval'] = count($approvalModel->getApproval());
+        $data['approval_proses'] = count($approvalModel->filterApproval(['status_approval'=>'proses']));
+        $data['approval_selesai'] = count($approvalModel->filterApproval(['status_approval'=>'selesai']));
         return view('dashboard',$data);
     }
 
--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -6,45 +6,75 @@ $this->section('content');
     <div class="row">
         <!-- Left side columns -->
         <div class="row">
-            <div class="col-xxl-4 col-md-6">
+            <?php
+            if(session()->get('level') != 'user'):
+            ?>
+            <div class="col-xxl-3 col-md-4">
                 <div class="card info-card sales-card">
                     <div class="card-body">
-                        <h5 class="card-title">Sales <span>| Today</span></h5>
+                        <h5 class="card-title">Petugas</h5>
                         <div class="d-flex align-items-center">
-                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-cart"></i></div>
-                            <div class="ps-3">
-                                <h6>145</h6> <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-person"></i></div>
+                            <div class="ps-2">
+                                <h6><?= number_format($petugas) ?></h6>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-xxl-4 col-md-6">
+            <div class="col-xxl-3 col-md-4">
                 <div class="card info-card sales-card">
                     <div class="card-body">
-                        <h5 class="card-title">Sales <span>| Today</span></h5>
+                        <h5 class="card-title">Penduduk Terdaftar</h5>
                         <div class="d-flex align-items-center">
-                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-cart"></i></div>
-                            <div class="ps-3">
-                                <h6>145</h6> <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-people"></i></div>
+                            <div class="ps-2">
+                                <h6><?= number_format($penduduk) ?></h6>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-xxl-4 col-md-6">
+            <div class="col-xxl-3 col-md-4">
                 <div class="card info-card sales-card">
                     <div class="card-body">
-                        <h5 class="card-title">Sales <span>| Today</span></h5>
+                        <h5 class="card-title">Semua Approval</h5>
                         <div class="d-flex align-items-center">
-                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-cart"></i></div>
-                            <div class="ps-3">
-                                <h6>145</h6> <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-card-text"></i></div>
+                            <div class="ps-2">
+                                <h6><?= number_format($approval) ?></h6> <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+            <div class="col-xxl-3 col-md-4">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Approval Diproses</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-card-text"></i></div>
+                            <div class="ps-2">
+                                <h6><?= number_format($approval_proses) ?></h6> <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xxl-3 col-md-4">
+                <div class="card info-card sales-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Approval Selesai</h5>
+                        <div class="d-flex align-items-center">
+                            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center"> <i class="bi bi-card-text"></i></div>
+                            <div class="ps-2">
+                                <h6><?= number_format($approval_selesai) ?></h6> <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php endif; ?>
         </div>
     </div>
 </section>
