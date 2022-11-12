81656f4ceee32886080dd678afd15b648e4a4911
Japar Sidik <japarssidik820@gmail.com>
2022-11-12 12:33:57 +0700

    Fix: Menghilangkan Angka pada Breadcrumb Uri

--- app/Views/layouts/app.php
+++ app/Views/layouts/app.php
@@ -12,17 +12,20 @@
             <?php
             $segments = current_url(true)->getSegments();
             array_splice($segments,0,1);
-            // echo count($segments);
+            if(is_numeric($segments[count($segments)-1])){
+                array_splice($segments,count($segments)-1,1);
+            }
             ?>
 
             <nav>
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="<?= base_url(current_url(true)->getSegments()[0].'/dashboard') ?>">Home</a></li>
                     <?php
+                    $base = (session()->get('level') != 'user')? 'main' : 'dktp' ;
                     for ($i = 0; $i < count($segments); $i++) :
                         $active_page = ($i===(count($segments)-1)) ? 'active' : '' ;
                         echo '<li class="breadcrumb-item '.$active_page.'">
-                        <a href="' . base_url('/ektp/') . '">'. ucwords($segments[$i]) .'</a>
+                        <a href="' . base_url("/$base/$segments[$i]") . '">'. ucwords($segments[$i]) .'</a>
                         </li>';
                     endfor;
                     ?>
